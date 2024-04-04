954a27ac234edeac3e7fa1dd9ea760213f5930c8
ProCoderHasan <165996515+ProCoderHasan@users.noreply.github.com>
2024-04-04 15:20:21 +0600

    Create DataTypes.sol
    dataType

---
+++ 100DaysOfSolidity/Day 2/DataTypes.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DataType {
+    string public name = "ProCoderHasan";
+    bool public myBl = true;
+    int public myInt = -123;
+    int public myInt2 = 123;
+    uint8 public myUint8 = 10;
+    uint256 public myUint256 = 2456777777;
+    address public myAddr = 0x96D41a358213693Db1C9fd1D5854a9D9f1F60c5f;
+    bytes public MyBt = hex"01";
+    bytes32 public myBt32 =  0x7465737400000000000000000000000000000000000000000000000000000000;
+}
